f74abd94ae24666a63a5a9a8cd969fed95b621a2
AnotherSola <38176179+anotherso1a@users.noreply.github.com>
2018-08-27 00:05:49 +0800

    Update DinoScript.js

--- DinoScript.js
+++ DinoScript.js
@@ -4,9 +4,9 @@ var s = document.createEvent('UIEvents')
 s.initUIEvent('keydown',true,true,window,0)
 s.keyCode=38
 var scp = setInterval(function(){
-	if(Math.max.apply(null,ctx.getImageData(110,112,40,1).data)>0){
+	if(Math.max.apply(null,ctx.getImageData(110,112,50,1).data)>0){
 		document.dispatchEvent(s)
-	}else if(Math.max.apply(null,ctx.getImageData(110,90,40,1).data)>0){
+	}else if(Math.max.apply(null,ctx.getImageData(110,90,50,1).data)>0){
 		document.dispatchEvent(s)
 	}
 },5)
